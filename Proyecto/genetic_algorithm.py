95dae9b6bb83493cc9c6bbc3afb53ca00b53b0ed
RodolfoFigueroa <rodfiso@ciencias.unam.mx>
2021-02-07 18:11:20 -0600

    Update genetic_algorithm.py

--- Proyecto/genetic_algorithm.py
+++ Proyecto/genetic_algorithm.py
@@ -13,8 +13,8 @@ def mutation_neural_net(neural_net, num_generation, I = 2):
     sigma = np.max(10 / np.log(num_generation * F), 1)
     W = [mutation_layer(layer.W, sigma, I) for layer in neural_net.layers]
     bias = [mutation_layer(layer.b, sigma, I) for layer in neural_net.layers]
-    activiation_functions = [layer.f for layer in neural_net.layers]
-    return neural_network(W, bias, activiation_functions, neural_net.f)
+    activation_functions = [layer.f for layer in neural_net.layers]
+    return neural_network(W, bias, activation_functions, neural_net.f)
 
 def crossover_product(neural_net_a, neural_net_b, t = 0.5):
     return ~ ((1 - t) * neural_net_a + t * neural_net_b )
